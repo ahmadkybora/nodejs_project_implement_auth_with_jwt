1b89b42890d9ceaba0d07fa5a248c6b018db0795
kybora <amontazeri53@gmail.com>
2021-06-26 12:21:19 +0430

    mew

---
+++ app/Models/ProductFavoriteLikeModel.js
@@ -0,0 +1,64 @@
+const {Sequelize, Model, DataTypes} = require("sequelize");
+const dbCon = require('../../database/connection');
+const ProductModel = require('./ProductModel');
+const UserModel = require('./UserModel');
+
+const ProductFavoriteLike = dbCon.define('ProductFavoriteLike', {
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        required: true,
+    },
+    /*productId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'products',
+            key: 'id'
+        },
+        onDelete: 'CASCADE',
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id'
+        },
+        onDelete: 'CASCADE',
+    },*/
+    isFavorite: {
+        type: DataTypes.BOOLEAN,
+    },
+    isLike: {
+        type: DataTypes.BOOLEAN,
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+    },
+    updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+    }
+});
+
+ProductModel.belongsToMany(UserModel, {
+    through: ProductFavoriteLike,
+    onDelete: 'CASCADE',
+    foreignKey: "productId",
+});
+
+UserModel.belongsToMany(ProductModel, {
+    through: ProductFavoriteLike,
+    onDelete: 'CASCADE',
+    foreignKey: "userId",
+});
+
+UserModel.hasMany(ProductFavoriteLike);
+ProductFavoriteLike.belongsTo(UserModel);
+ProductModel.hasMany(ProductFavoriteLike);
+ProductFavoriteLike.belongsTo(ProductModel);
+
+module.exports = ProductFavoriteLike;
